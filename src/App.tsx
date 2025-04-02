8c5f4bf96776fe23ec72e4498cc40be9a4455eeb
Antonio Martin <luis.martinf89@gmail.com>
2025-04-01 20:56:37 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import Hero from "./Components/Hero/Hero";
 import NavBar from "./Components/Shared/NavBar/NavBar";
@@ -10,6 +11,20 @@ import Aurora from "./Components/Shared/Aurora/Aurora";
 
 const Home = () => {
     const { i18n } = useTranslation();
+    const [cursorEnabled, setCursorEnabled] = useState(true);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 480px)");
+
+        const handleMediaChange = (e: MediaQueryListEvent | MediaQueryList) => {
+            setCursorEnabled(!e.matches);
+        };
+
+        handleMediaChange(mediaQuery);
+        mediaQuery.addEventListener("change", handleMediaChange);
+
+        return () => mediaQuery.removeEventListener("change", handleMediaChange);
+    }, []);
 
     const changeLanguage = (lng: "en" | "es" | "fr") => {
         i18n.changeLanguage(lng);
@@ -17,45 +32,47 @@ const Home = () => {
 
     return (
         <>
-            <AnimatedCursor
-                innerSize={8}
-                trailingSpeed={1}
-                outerSize={8}
-                color='192, 215, 213'
-                outerAlpha={0.2}
-                innerScale={0.7}
-                outerScale={5}
-                showSystemCursor={false}
-                outerStyle={{
-                    border: "1px solid var(--rich-black)",
-                    zIndex: 9999,
-                }}
-                innerStyle={{
-                    backgroundColor: "192, 215, 213",
-                    zIndex: 9999,
-                }}
-                clickables={[
-                    "a",
-                    'input[type="text"]',
-                    'input[type="email"]',
-                    'input[type="number"]',
-                    'input[type="submit"]',
-                    'input[type="image"]',
-                    "input",
-                    "label[for]",
-                    "select",
-                    "textarea",
-                    ".contact-item",
-                    ".project",
-                    ".container-download",
-                    ".swiper-button-next",
-                    ".swiper-button-prev ",
-                    ".hamburger",
-                    ".logo",
-                    "button",
-                    ".link",
-                ]}
-            />
+            {cursorEnabled && (
+                <AnimatedCursor
+                    innerSize={8}
+                    trailingSpeed={1}
+                    outerSize={8}
+                    color="192, 215, 213"
+                    outerAlpha={0.2}
+                    innerScale={0.7}
+                    outerScale={5}
+                    showSystemCursor={false}
+                    outerStyle={{
+                        border: "1px solid var(--rich-black)",
+                        zIndex: 9999,
+                    }}
+                    innerStyle={{
+                        backgroundColor: "192, 215, 213",
+                        zIndex: 9999,
+                    }}
+                    clickables={[
+                        "a",
+                        'input[type="text"]',
+                        'input[type="email"]',
+                        'input[type="number"]',
+                        'input[type="submit"]',
+                        'input[type="image"]',
+                        "input",
+                        "label[for]",
+                        "select",
+                        "textarea",
+                        ".contact-item",
+                        ".project",
+                        ".container-download",
+                        ".swiper-button-next",
+                        ".swiper-button-prev ",
+                        ".hamburger",
+                        ".logo",
+                        "button",
+                        ".link",
+                    ]}
+                />
+            )}
             <NavBar />
             <Hero changeLanguage={changeLanguage} />
             <Aurora colorStops={["#c0d7d5", "#418383", "#a6cbca"]} speed={1} />
